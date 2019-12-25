45d9cc817aa40a21977f0a31404b35db66f60e8a
aminedammak <amine.dammak@softtodo.com>
2019-12-25 16:57:19 +0100

    added form for user registration

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .Form import RegisterFormUser
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterFormUser(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'account created for {username}')
+            return redirect('blog-home')
+    else:
+        form = RegisterFormUser()
+    return render(request, "users/registerForm.html", {'form': form})
