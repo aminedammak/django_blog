8096bf308dd38f3942c11f1fdec63cdb277d74c8
aminedammak <amine.dammak@softtodo.com>
2019-12-31 15:02:33 +0100

    Post update

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostsListView, PostDetailView, PostDeleteView, PostCreateView
+from .views import PostsListView, PostDetailView, PostDeleteView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='blog-home'),
     path('post-detail/<pk>', PostDetailView.as_view(), name='post-detail'),
     path('post-delete/<pk>', PostDeleteView.as_view(), name='post-delete'),
     path('post-create/', PostCreateView.as_view(), name='post-create'),
+    path('post-update/<pk>', PostUpdateView.as_view(), name='post-update'),
     path('about/', views.about, name='blog-about')
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, DeleteView, CreateView
+from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
 
 posts = Post.objects.all()
 
@@ -41,6 +41,22 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    success_url = "/"
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
