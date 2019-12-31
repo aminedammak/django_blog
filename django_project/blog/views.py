b9e6439660942057534bc8aae1800f8555c9c7c1
aminedammak <amine.dammak@softtodo.com>
2019-12-31 11:28:42 +0100

    Allow posts deletion to only the logged user and the owner of the post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, DeleteView
 
 posts = Post.objects.all()
@@ -21,9 +22,16 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return HttpResponse("<h2>About</h2>")
